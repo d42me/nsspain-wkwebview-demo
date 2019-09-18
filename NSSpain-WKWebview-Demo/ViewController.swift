08cb5768b4c138d6721e071ad54b400e940bb9e4
Dominik Scherm <dominik@dnddev.com>
2019-09-18 19:48:08 +0200

    Initial commit.

--- NSSpain-WKWebview-Demo/ViewController.swift
+++ NSSpain-WKWebview-Demo/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addressBarTextField: UITextField!
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        webView.navigationDelegate = self
+        webView.load(URLRequest(url: URL(string: "https://nsspain.com")!))
+        
+    }
+    
+    
+    @IBAction func submitURL(_ sender: Any) {
+        webView.load(URLRequest(url: URL(string: addressBarTextField.text!)!))
     }
 
+}
 
+extension ViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        addressBarTextField.text = webView.url?.absoluteString
+        activityIndicator.stopAnimating()
+        activityIndicator.isHidden = true
+    }
 }
 
